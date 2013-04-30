71d7a4c3b0cb229a84fda8d1a78434ad19e37ef4
Daniel Menche <menche_mt@yahoo.com>
2013-04-30 13:29:41 -0700

    check for lava containment

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ lava_aircool
 ============
 
 Makes flowing lava cool by itself if too far from a source
+Lava that is contained will *usually* not cool. The mod checks for containment only by counting the number of solid nodes around it, so it isn't perfect. To prevent accidental spills, I tried to err on the side of too much cooling.
--- init.lua
+++ init.lua
@@ -3,8 +3,9 @@ minetest.register_abm({
 		neighbors = {"air"},
 		interval = 1.0,
 		chance = 1,
-		action = function(pos, node, active_object_count, active_object_count_wider)
-			if minetest.env:find_node_near(pos, 2, "default:lava_source") == nil then
+		action = function(pos, node, active_object_count, active_object_count_wider) 
+			if minetest.env:find_node_near(pos, 1, "default:lava_source") == nil or
+			# minetest.env:find_nodes_in_area({pos.x-1, pos.y-1, pos.z-1}, {pos.x+1, pos.y+2, pos.z+1}, "air", "default:lava_flowing") > 12 then
 				minetest.env:set_node(pos, {name="default:stone"}) ;
 			end
 		end,
