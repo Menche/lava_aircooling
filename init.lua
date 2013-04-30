1252159dd118fb020111a91b644e4ffa4235c503
Daniel Menche <menche_mt@yahoo.com>
2013-04-30 15:26:27 -0700

    get cooling radius from minetest.conf, default to 2

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
+local radius = minetest.setting_get("lava_cooling_radius")
+
+if radius == nil then
+	radius = 2
+end
+
 minetest.register_abm({
 		nodenames = {"default:lava_flowing"},
 		neighbors = {"air"},
 		interval = 1.0,
 		chance = 1,
 		action = function(pos, node, active_object_count, active_object_count_wider) 
-			if minetest.env:find_node_near(pos, 1, "default:lava_source") == nil or
+			if minetest.env:find_node_near(pos, radius, "default:lava_source") == nil or
 			# minetest.env:find_nodes_in_area({pos.x-1, pos.y-1, pos.z-1}, {pos.x+1, pos.y+2, pos.z+1}, "air", "default:lava_flowing") > 12 then
 				minetest.env:set_node(pos, {name="default:stone"}) ;
 			end
